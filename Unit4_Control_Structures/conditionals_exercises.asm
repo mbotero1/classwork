d6532d35ad4ac93a8465fc04b76f2853bc738242
Matias <157539993+mbotero1@users.noreply.github.com>
2024-02-27 21:07:46 +0000

    conditional exercises

--- Unit4_Control_Structures/conditionals_exercises.asm
+++ Unit4_Control_Structures/conditionals_exercises.asm
@@ -6,24 +6,53 @@
 %include "asm_io.inc"
 
 
-segment .data					; initialized data
+segment .data ; initialized data
+        if_prompt db "Enter a character: ", 0
+        if_output db "and... your point is?", 0
+
+        number_prompt db "Enter a number: ", 0
+        odd           db " is odd", 0
+        even          db " is even", 0
+
+segment .bss ; uninitialized data
+        number resd 1
+        
 
 
-segment .bss					; uninitialized data
-
-
-
-segment .text					; code
-        global  asm_main
+segment .text ; code
+        global asm_main
 asm_main:
-        enter   0,0            	; setup routine
+        enter 0, 0 ; setup routine
         pusha
 
-	; TODO: IF EXERCISE
+; 	; TODO: IF EXERCISE
+;         mov  eax, if_prompt
+;         call print_string
+;         call read_char
 
+;         cmp  eax, "&"
+;         jne  endif
+;         mov  eax, if_output
+;         call print_string
 
+; endif:
+;         call read_char
 	; TODO: IF/ELSE EXERCISE
 
+        mov  eax, number_prompt
+        call print_string
+        call read_int
+
+        and eax, 1
+
+        cmp eax, 0
+        je  even_block
+        mov eax, odd
+        jmp end_ifelse
+even_block:
+        mov eax, even
+end_ifelse:
+        call print_string
 
 	; TODO: ELIF EXERCISE
 
@@ -31,6 +60,7 @@ asm_main:
 	; TODO: AND EXERCISE
 
         popa
-        mov     eax, 0        	; return back to C
+        mov eax, 0 ; return back to C
         leave                     
         ret
+
--- Unit4_Control_Structures/loops_exercises.asm
+++ Unit4_Control_Structures/loops_exercises.asm
@@ -4,25 +4,84 @@
 ; Purpose - to learn about loops in NASM
 
 %include "asm_io.inc"
-segment .data						; initialized data
+segment .data ; initialized data
+        rocket db "🚀!", 0
+        limit  dd 5
+
+        perfect_prompt         db "Enter a power of 2: ", 0
+        perfect                db "PERFECT", 0
+        another_perfect_prompt db "Again!", 0
+
+        fear_the_goat   db "Fear the 🐐!", 0
+        response_prompt db "Response? ", 0
 
 
-segment .bss						; uninitialized data
+segment .bss  ; uninitialized data
 
-
-segment .text						; code
-        global  asm_main
+segment .text ; code
+        global asm_main
 asm_main:
-        enter   0,0               	; setup routine
+        enter 0, 0 ; setup routine
         pusha
+
 
 		; TODO: FOR EXERCISE
 
+        ; mov ecx, [limit]
+
+        ; for:
+        ;         mov  eax, ecx
+        ;         call print_int
+        ;         call print_nl
+        ;         loop for
+        
+        ; mov  eax, rocket
+        ; call print_string
+        ; call print_nl
+
+
 		; TODO: WHILE EXERCISE
 
+        ; mov  eax, perfect_prompt
+        ; call print_string
+        ; while:
+        ;         call read_int
+                
+        ;         mov ebx, eax
+        ;         sub ebx, 1
+        ;         and ebx, eax
+        ;         cmp ebx, 0
+        ;         jne end_while
+                
+        ;         mov  eax, perfect
+        ;         call print_string
+        ;         call print_nl
+
+        ;         mov  eax, another_perfect_prompt
+        ;         call print_string
+        ;         call print_nl
+
+        ;         jmp while
+
+        ; end_while:
+
 		; TODO: DO WHILE EXERCISE
+        do:
+                call read_char
+                mov  eax, fear_the_goat
+                call print_string
+                call print_nl
+
+                mov  eax, response_prompt
+                call print_string
+                call read_char
+
+                cmp eax, "!"
+                jne do
+        
+
 
         popa
-        mov     eax, 0            ; return back to C
+        mov eax, 0 ; return back to C
         leave                     
         ret
